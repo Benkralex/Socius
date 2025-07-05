4d6bc38d33028a8c1bc32479abaf21e10fa146a4
Benkralex <75564514+Benkralex@users.noreply.github.com>
2025-07-05 18:17:17 +0200

    Remove unused LocalContext import from Events.kt and Relations.kt

--- app/src/main/java/de/benkralex/socius/widgets/contactInfromationWidgets/Events.kt
+++ app/src/main/java/de/benkralex/socius/widgets/contactInfromationWidgets/Events.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import de.benkralex.socius.backend.ContactEvent
 import de.benkralex.socius.backend.settings.getFormattedDate
