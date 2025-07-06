d810fbff81936a52c329eab854ded6e39de20ea6
Benkralex <75564514+Benkralex@users.noreply.github.com>
2025-07-06 17:34:09 +0200

    Update ContactsListHeading.kt

--- app/src/main/java/de/benkralex/socius/widgets/ContactsListHeading.kt
+++ app/src/main/java/de/benkralex/socius/widgets/ContactsListHeading.kt
@@ -4,14 +4,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
