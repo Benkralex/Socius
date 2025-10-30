08c18296b3670a3981bb6cef64ec2976de6ba352
Benkralex <ben_github@enkrich.de>
2025-10-30 22:55:42 +0100

    Clean up

--- app/src/main/java/de/benkralex/socius/data/contacts/local/database/LocalContactsEntity.kt
+++ app/src/main/java/de/benkralex/socius/data/contacts/local/database/LocalContactsEntity.kt
@@ -49,37 +49,6 @@ data class LocalContactsEntity (
 
     var customFields: Map<String, String> = emptyMap()
 ) {
-    companion object {
-        fun fromContact(contact: Contact): LocalContactsEntity {
-            return LocalContactsEntity(
-                prefix = contact.prefix,
-                givenName = contact.givenName,
-                middleName = contact.middleName,
-                familyName = contact.familyName,
-                suffix = contact.suffix,
-                nickname = contact.nickname,
-                phoneticGivenName = contact.phoneticGivenName,
-                phoneticMiddleName = contact.phoneticMiddleName,
-                phoneticFamilyName = contact.phoneticFamilyName,
-                organization = contact.organization,
-                department = contact.department,
-                jobTitle = contact.jobTitle,
-                note = contact.note,
-                isStarred = contact.isStarred,
-                photoUri = contact.photoUri,
-                thumbnailUri = contact.thumbnailUri,
-                phoneNumbers = contact.phoneNumbers,
-                emails = contact.emails,
-                addresses = contact.addresses,
-                websites = contact.websites,
-                relations = contact.relations,
-                events = contact.events,
-                groups = contact.groups,
-                customFields = contact.customFields
-            )
-        }
-    }
-
     fun toContact(context: Context): Contact {
         val contentResolver = context.contentResolver
         val photoBitmap = if (photoUri != null) {
--- app/src/main/java/de/benkralex/socius/widgets/ContactsList.kt
+++ app/src/main/java/de/benkralex/socius/widgets/ContactsList.kt
@@ -29,12 +29,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalHapticFeedback
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
